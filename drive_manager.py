c6b3eaa8780624a4a64e81795bf3a4be97f8f52c
Eladir-Dev <ejmatosperez@gmail.com>
2026-09-09 22:48:39 -0400

    Some empty functions were added to drive_manager.py to be later finished

--- drive_manager.py
+++ drive_manager.py
@@ -15,11 +15,40 @@ Last modification date: 9/sep/2026
 Last modified by: Eduardo J. Matos
 -----------------------------------------------------------------
 Last modifications:
-    * Somthihng
-        - Description of the modification
+    *  authenticate_drive, upload_file, and download_file were added.
+        - They are going to be finished at later date.
     * 
 ================================================================
 """
 #============Imports============#
 
 #=========Code Starts===========#
+
+def authenticate_drive():
+    """
+    Authenticates the application with Google Drive API.
+    Returns the authenticated service object.
+    """
+    # Authentication logic here
+    pass
+
+def upload_file(file_path, folder_id):
+    """
+    Uploads a file to Google Drive.
+
+    :param file_path: Path to the local file to be uploaded
+    :param folder_id: ID of the folder where the file will be uploaded
+    """
+    # File upload logic here
+    pass
+
+def download_file(folder_id, file_name, destination_path):
+    """
+    Downloads a file from Google Drive.
+
+    :param folder_id: ID of the folder containing the file
+    :param file_name: Name of the file to be downloaded
+    :param destination_path: Local path where the file will be saved
+    """
+    # File download logic here
+    pass
